d18a9f57f47fe802a51fcf02f2212d22b920d6ab
Ajay <ajaykh053@gmail.com>
2026-09-20 16:32:07 +0530

    add psycopg and modify main and requirements.txt

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 
 # Optional: add contact me email functionality (Day 60)
 import os
+import psycopg
 import smtplib
 from dotenv import load_dotenv
 
